95da8cd1efaf3db7a0b66e5cc87376f52d5acae1
FaaZzZ <daffavaiz2008@gmail.com>
2024-08-06 20:11:50 +0700

    Update config.js

--- setting/config.js
+++ setting/config.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-global.owner = ["6285742632270"];
-global.author = "RezaDevv";
+global.owner = ["6288277449869"];
+global.author = "FAMIKU STORE";
 global.packname = "Push-Kontak";
 global.sessionName = "Bot-Session";
 global.versionbot = "1.2.0";
